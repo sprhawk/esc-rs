b35192b1efef0ece41af08c567b0d4661ff3b88f
sprhawk <465558+sprhawk@users.noreply.github.com>
2020-08-27 21:02:29 +0800

    output print via semihosting

--- src/main.rs
+++ src/main.rs
@@ -23,11 +23,13 @@ use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch
 
 use cortex_m::asm;
 use cortex_m_rt::entry;
+use cortex_m_semihosting::{debug, hprintln};
 
 #[entry]
 fn main() -> ! {
     asm::nop(); // To not have main optimize to abort in release mode, remove when you add code
 
+    hprintln!("Hello World").unwrap();
     loop {
         // your code goes here
     }
